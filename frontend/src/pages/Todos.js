ba71595a5532afb7dabf6c176919f55709f9e57c
dansbands <dansbands@hotmail.com>
2020-06-11 17:38:35 -0400

    styled todos

--- frontend/src/pages/Todos.js
+++ frontend/src/pages/Todos.js
@@ -2,27 +2,32 @@ import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import Layout from "../components/Layout";
 import { withRouter } from "react-router-dom";
+import { TodoList, TodoCard } from "../styles";
 
 const Todos = (props) => {
   const [todos, setTodos] = useState([]);
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/todos/")
+    getTodos();
+  }, []);
+
+  const getTodos = () => {
+    return fetch("https://jsonplaceholder.typicode.com/todos/")
       .then((response) => response.json())
       .then((json) => setTodos(json));
-  }, []);
+  }
 
   const renderTodos = () => {
     return todos
       .filter((todo) => todo.userId === 1)
-      .map((todo) => <div>{todo.title}</div>);
+      .map((todo) => <TodoCard key={todo.id}>{todo.title}</TodoCard>);
   };
 
   return (
     <>
       <Layout title="Todos">
         <h3>Todos</h3>
-        {renderTodos()}
+        <TodoList>{renderTodos()}</TodoList>
       </Layout>
     </>
   );
--- frontend/src/styles/index.js
+++ frontend/src/styles/index.js
@@ -5,3 +5,15 @@ export const StyledLink = styled(Link)`
   margin-left: 10px;
   text-decoration: none;
 `;
+
+export const TodoList = styled.div`
+  margin: 0 10%;
+`;
+
+export const TodoCard = styled.div`
+  text-align: left;
+  padding: 10px;
+  border: 1px solid lightgrey;
+  border-radius: 5px;
+  margin: 2px 0;
+`;
